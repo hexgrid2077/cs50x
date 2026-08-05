add491b8b7e73973cd79438ae9d20feba8794b8b
Mike <synthetica.alt@proton.me>
2026-08-04 23:23:19 -0600

    Finished the arrays chapter, now onto the problems after these shorts and Section.

---
+++ 2/class_notes/greet.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc ==2)
+    {
+    // Will say hello to the first line of your command
+    printf("Hello, %s\n", argv[1]);
+    }
+    else
+    printf("Hello.\n");
+}
