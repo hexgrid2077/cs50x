fef9726ed3ed4e22bdd0574aa284f84e80bad188
Mike <synthetica.alt@proton.me>
2026-08-09 22:53:01 -0600

    Finished the lecture on Arrays (Week '2'). Onto the Session and shorts then problems. This one was a brain twister.

---
+++ 3_Algorithms/class_notes/recursion.c
@@ -0,0 +1,31 @@
+// #include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+
+int main(void)
+{
+    // int height = get_int("Height: ");
+    int height = 4;
+
+    draw(height);
+}
+
+void draw(int n)
+{
+    // Base case
+    if (n <= 0)
+    {
+        return;
+    }
+    
+    // Print a pyramid of height n - 1
+    draw(n - 1);
+
+    // Print one more row
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
