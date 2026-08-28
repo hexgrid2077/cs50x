0cf162d948a871d892d25c0137cb5a3757da6a9d
Mike <synthetica.alt@proton.me>
2026-08-28 00:11:12 -0600

    Working on week 4

--- 4_memory/class_notes/compare.c
+++ 4_memory/class_notes/compare.c
@@ -5,14 +5,16 @@
 int main(void)
 {
     char* s = get_string("s: ");
-    char* t = get_string("t: ");
+    char*  t = get_string("t: ");
 
-    if (strcmp(s, t) == 0)
-    {
-        printf("Same\n");
-    }
-    else
-    {
-        printf("Different\n");
-    }
+    // if (strcmp(s, t) == 0)
+    // {
+    //     printf("Same\n");
+    // }
+    // else
+    // {
+    //     printf("Different\n");
+    // }
+    printf("%p\n", s);
+    printf("%p\n", t);
 }
---
+++ 4_memory/class_notes/copy.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char* s = get_string("s: ");
+
+    char* t = s;
+
+    t[0] = toupper(*t);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
